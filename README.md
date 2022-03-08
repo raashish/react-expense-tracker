09ff4c4aa2d5e04eeb8d504041e960483140264a
raashish <rajguruaashish@gmail.com>
2022-03-08 23:45:50 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-furyte
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-furyte)
---
+++ src/context/GlobalState.js
@@ -0,0 +1,46 @@
+import React, { createContext, useReducer } from 'react';
+import { AppReducer } from './AppReducer';
+
+//Initial State
+const initialState = {
+  transactions: [
+    { id: 1, text: 'Flower', amount: -20 },
+    { id: 2, text: 'Salary', amount: 300 },
+    { id: 3, text: 'Book', amount: -10 },
+    { id: 4, text: 'Camera', amount: 160 },
+  ],
+};
+
+//Create context
+export const GlobalContext = createContext(initialState);
+
+//Provider components
+export const GlobalProvider = ({ children }) => {
+  //Actions
+  function deleteTransaction(id) {
+    dispatch({
+      type: 'DELETE_TRANSACTION',
+      payload: id,
+    });
+  }
+
+  function addTransaction(transaction) {
+    dispatch({
+      type: 'ADD_TRANSACTION',
+      payload: transaction,
+    });
+  }
+
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+  return (
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
